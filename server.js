e9f20c755e1a6bb9851b170b3e72f17e446eff22
gkarakus <karakus_g@yahoo.com>
2021-09-10 14:20:00 -0400

    eskiyedon

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 //var http = require('http');
-//var port = process.env.PORT || 1938;
+var port = process.env.PORT || 3000;
 
 var express = require('express');
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(port);
 
 app.use(express.static('public'));
 
