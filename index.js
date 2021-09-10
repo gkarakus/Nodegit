f35a98922f17f0940a23e0fd005916634f4a14c4
gkarakus <karakus_g@yahoo.com>
2021-09-10 00:29:05 -0400

    mdade change

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const WebSocketServer = require("Websocket").server
-
-const httpserver = http.createServer((req, res) =>{ 
-
-    console.log("we have server");
-})
-
-httpserver.listen(8080, () => console.log("my server listening "))
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-var http = require('http');
-var dt = require('./myfirstmodule');
+const http = require('http');
+const express = require('express');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.write("baskanedir");  
-  res.end();
-}).listen(8080);
+const WebSocket = require('ws');
 
+const port = 6969;
+const server = http.createServer(express);
+const wss = new WebSocket.Server({ server })
 
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    })
+  })
+})
 
-
-
+server.listen(port, function() {
+  console.log('Server is listening on ')
+})
 
 
