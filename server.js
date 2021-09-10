812751d538064b9417f2ebd4260abf11189de5a0
gkarakus <karakus_g@yahoo.com>
2021-09-10 04:58:49 -0400

    drawboard

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
+//var http = require('http');
+var express = require('express');
+var app = express();
+var server = app.listen(8080);
+
+app.use(express.static('public'));
+
+console.log("My server basladi");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+
+function newConnection(socket) {
+   
+   //console.log(socket);
+   console.log('new connection: ' + socket.id);
+
+   socket.on('mouse', mouseMsg);
+
+   function mouseMsg(data) {
+      socket.broadcast.emit('mouse', data);
+     // console.log(data);
+   }
+
+}
+
+
+//var fs = require('fs');
+//http.createServer(function (req, res) {
   //Open a file on the server and return its content:
-  fs.readFile('jsdene.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+  //fs.readFile('jsdene.html', function(err, data) {
+  //  res.writeHead(200, {'Content-Type': 'text/html'});
+  //  res.write(data);
+  //  return res.end();
+ // });
+//}).listen(8080);
 
